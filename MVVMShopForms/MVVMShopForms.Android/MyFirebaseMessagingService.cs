132b643ea9c5489a05939fb8341ed61824fee5ab
Yael Chavez <you400@gmail.com>
2021-08-23 20:54:15 -0500

    Push Notification 1

---
+++ MVVMShopForms/MVVMShopForms.Android/MyFirebaseMessagingService.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Firebase.Messaging;
+
+namespace MVVMShopForms.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+
+        public override void OnMessageReceived(RemoteMessage p0)
+        {
+            base.OnMessageReceived(p0);
+        }
+    }
+}
